e3a24efb67942b45b735da2dd1d2e833d1116e36
itsmathewtj <mathewtj2112@gmail.com>
2025-11-12 14:36:07 +0530

    Updatedt

--- js/main.js
+++ js/main.js
@@ -119,3 +119,11 @@
     
 })(jQuery);
 
+window.addEventListener("scroll", function () {
+    const nav = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
+    }
+  });
